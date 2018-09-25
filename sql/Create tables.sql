b56ddbf42e9a633c2bae0594871283165781a856
liamjm13 <42192141+liamjm13@users.noreply.github.com>
2018-09-25 15:01:17 +1200

    Add files via upload

--- sql/Create tables.sql
+++ sql/Create tables.sql
@@ -1,6 +1,10 @@
 use [StudyApp]
 go
 
+if OBJECT_ID('enrolment','u') is not null
+drop table enrolment
+go
+
 if OBJECT_ID('course','u') is not null
 drop table course
 go
@@ -11,19 +15,40 @@ go
 
 create table student
 (
-	student_id int identity(1,1) primary key not null,
-	student_name varchar(25) not null,
-	age int,
-	address varchar(25),
-	phone_number varchar(25),
+	student_id int identity(1,1) not null,
+	name varchar(25) null,
+	age int not null,
+	phone nvarchar (30) null
+
+	PRIMARY KEY CLUSTERED (student_id ASC)
 )
 
 create table course
 (
-	course_id int identity(1,1) primary key not null,
-	course_name varchar(20) not null,
+	course_id nvarchar(20) not null,
+	course_name nvarchar(30) not null,
+	description varchar(50) null,
+	year int null,
+	semester int null,
+
+	PRIMARY KEY CLUSTERED (course_id ASC)
+)
+
+create table enrolment
+(
+	enrol_id int identity (1, 1) not null,
+	student_id int not null,
+	course_id nvarchar(20) not null,
+	enrolment_year int not null,
+	enrolment_semester int not null,
+
+	CONSTRAINT [PK_dbo.Enrolment] PRIMARY KEY CLUSTERED (enrol_id ASC),
+	CONSTRAINT [FK_StdEnrol] FOREIGN KEY (student_id) REFERENCES dbo.Student (student_id),
+	CONSTRAINT [FK_CourseEnrol] FOREIGN KEY (course_id) REFERENCES dbo.Course (course_id)
 )
 
 select * from student
 
 select * from course
+
+select * from enrolment
