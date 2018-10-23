ebdd7665712c2990f48b71beac306b6e428d2717
liamjm13 <42192141+liamjm13@users.noreply.github.com>
2018-10-23 14:01:49 +1300

    Add files via upload

--- sql/Sql querys/Create tables.sql
+++ sql/Sql querys/Create tables.sql
@@ -25,27 +25,44 @@ create table student
 
 create table course
 (
-	course_id nvarchar(20) not null,
-	course_name nvarchar(30) not null,
+	code nvarchar(20) not null,
+	name nvarchar(30) not null,
 	description varchar(50) null,
-	year int null,
-	semester int null,
+	semester_offered varchar(15) null,
+	pre_requisite_course_code int null,
+	compulsory char not null
 
-	PRIMARY KEY CLUSTERED (course_id ASC)
+	PRIMARY KEY CLUSTERED (code ASC)
 )
 
 create table enrolment
 (
-	enrol_id int identity (1, 1) not null,
+	enrol_id int identity (1000000, 1) not null,
 	student_id int not null,
-	course_id nvarchar(20) not null,
+	code nvarchar(20) not null,
 	enrolment_year int not null,
-	enrolment_semester int not null,
+	enrolment_semester varchar(15) not null,
 
 	CONSTRAINT [PK_dbo.Enrolment] PRIMARY KEY CLUSTERED (enrol_id ASC),
 	CONSTRAINT [FK_StdEnrol] FOREIGN KEY (student_id) REFERENCES dbo.Student (student_id),
-	CONSTRAINT [FK_CourseEnrol] FOREIGN KEY (course_id) REFERENCES dbo.Course (course_id)
+	CONSTRAINT [FK_CourseEnrol] FOREIGN KEY (code) REFERENCES dbo.Course (code)
 )
+
+insert into student values
+('Jack Deadoor', 19, '0800-838383'),
+('Luko Karsandra', 26, '0159-357-654456')
+
+insert into course values
+(100001, 'Building', 'Build the thing', '1-4', 323, 'N'),
+(100002, 'Eugenics Program', 'Help breed the superior race', '1-4', 001, 'Y'),
+(100003, 'Genocide program', 'Help destroy the inferior races', '1-4', 002, 'Y')
+
+insert into enrolment values
+(01, 100002, 2066, '1-4'),
+(02, 100002, 2066, '1-4'),
+(02, 100001, 2066, '1-4'),
+(01, 100003, 2066, '1-4'),
+(02, 100003, 2066, '1-4')
 
 select * from student
 
